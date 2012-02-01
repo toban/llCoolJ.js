da2b35210139a2cc79e51bb89758bccab9f02dd5
Tobias Andersson <tobias.ande@gmail.com>
2012-02-01 11:39:19 +0100

    added optional hostname and llcoolj hosts in order to make the getting of llcoolj more reliable with redundancy.

--- jquery.llcoolj.js
+++ jquery.llcoolj.js
@@ -15,14 +15,84 @@
 		this.append("<div id='llCoolJ'></div>");
 		var settings = $.extend( {
 	      'inset'       : false,
-	      'interactive' : false
+	      'interactive' : false,
+	      'host'	: null
 	    }, options);
+	    
+	    	var coolHosts = new Array("likeneveralways.com","kthx.pl");
+	    	var prefix = "http://";
+	    	var postfix = "/images/"
+	    	var imgsrc = "llcoolj.jpg";
+	    	var hostIndex = coolHosts.length-1;
+	    	var timeoutMs = 2000;
+	    	var useTextVersion = false;
+	    	
+    		if(settings['host']!=null)
+	    	{
+	   	 	coolHosts.push(settings['host']);
+	   	 	var hostIndex = coolHosts.length-1;
+	    	}
+	    	
+    		var isLoaded = false;
+    		
+    		// load the image
+    		loadImg = function()
+    		{
+			var coolj = $("<img id='llCoolJImg' />").attr('src', prefix+coolHosts[hostIndex]+postfix+imgsrc)
+			.load(function() {
+			if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) 
+			{
+				isLoaded = false;
+			} 
+			else 
+			{
+				isLoaded = true;
+				$("#llCoolJ").append(coolj);
+			}
+			});
+			
+			// every timeoutMs check if the image is loaded, else skip to next host
+			setTimeout(loadImgCheck,timeoutMs);
+    		}
+    		
+    		// check if the image is loaded
+		loadImgCheck = function()
+		{
+			if(!isLoaded)
+			{
+				hostIndex--;
+				if(hostIndex < 0)
+				{
+					// no more hosts, use text
+					isLoaded = true;
+					useTextVersion = true;
+					
+				}
+				else
+				{
+					loadImg();
+				}
+			}
+			else
+			{
+				
+			}
+		};
+		loadImg();
+		
+		if(useTextVersion)
+		{
+			$('#llCoolJ').addClass('inset');
+		}
+			
+	    	
+	    	
 		
 		if(settings['inset']){
 			$('#llCoolJ').addClass('inset');
 			$('#overlay').css('background', 'none');
 			if(settings['interactive']){
-				$('#llCoolJ').click(function(){
+				$('#llCoolJImg').click(function(){
 					console.log($(this).css('bottom'));
 					if($(this).css('bottom') != "0px"){
 							$(this).animate({
